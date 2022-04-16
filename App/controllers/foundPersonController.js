827a7e11ad0489ba0a031835c370172a4e1e4888
Juhan <davidjuhan23@gmail.com>
2022-04-16 23:32:15 +0600

    server updated

--- App/controllers/foundPersonController.js
+++ App/controllers/foundPersonController.js
@@ -1,31 +1,36 @@
+const { default: axios } = require("axios");
+const { json } = require("express/lib/response");
 const FoundPerson = require("../models/foundPersonModel.js");
 const catchAsyncError = require("../Utils/catchAsyncError.js");
 const { cloundinary } = require("../Utils/cloudinary.js");
 const ErrorHandler = require("../Utils/Errorhandler.js");
 
 exports.createfoundPerson = catchAsyncError(async (req, res, next) => {
-  const {
-    name,
-    photo,
-    address,
-    phone
-  } = req.body;
+  res.status(200).json({
+    status: "success",
+  });
+  // console.log(req, 'res111')
+  // const {
+  //   name,
+  //   photo,
+  //   address,
+  //   phone
+  // } = req.body;
 
-  cloundinary.uploader.upload(photo, async (err, result) => {
-    if (err) return next(new ErrorHandler(err.message, 400));
-    const foundPerson = await FoundPerson.create({
-      name,
-      photo: result.secure_url,
-      address,
-      phone
-    });
-
-    res.status(201).json({
-      success: true,
-      foundPerson
-    });
-  }
-  );
+  // cloundinary.uploader.upload(photo, async (err, result) => {
+  //   if (err) return next(new ErrorHandler(err.message, 400));
+  //   const foundPerson = await FoundPerson.create({
+  //     name,
+  //     photo: result.secure_url,
+  //     address,
+  //     phone
+  //   });
+  //   res.status(201).json({
+  //     success: true,
+  //     foundPerson
+  //   });
+  // }
+  // );
 })
 
 exports.getAllFoundPerson = catchAsyncError(async (req, res, next) => {
